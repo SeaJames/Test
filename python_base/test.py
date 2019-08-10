5747a24c076b7f8f9f4ecc220c7351939cbb2d91
seaJames <1716190289@qq.com>
2019-08-10 17:40:15 +0800

    adjust test

--- python_base/test.py
+++ python_base/test.py
@@ -3,5 +3,24 @@
 Author: 17161
 Date: 2019/8/10
 """
+
+
+def wrapper(func):
+    print("welcome to wrapper")
+
+    def do_func(*args, **kwargs):
+        print("this is do_func")
+        func(*args, **kwargs)
+        print("func run finished")
+    print("this is the leave")
+    return do_func
+
+
+@wrapper
+def run(name, age):
+    print("{} : {}".format(name, age))
+
+
 if __name__ == '__main__':
-    print("this is my first test python on xiaomi pc")
+    # print("this is my first test python on xiaomi pc")
+    run("zhansan", 12)
